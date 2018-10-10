8188339554d09f696b946530e9da8f4bf2f08d1e
Vedran Pavic <vedran.pavic@gmail.com>
2018-10-10 20:34:53 +0200

    Leverage Spring Boot auto-config in OAuth2 client demo

--- demo-client/src/main/java/demo/SecurityConfiguration.java
+++ demo-client/src/main/java/demo/SecurityConfiguration.java
@@ -1,15 +1,10 @@
 package demo;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -21,18 +16,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
                 .anyRequest().authenticated();
         // @formatter:on
-    }
-
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(google());
-    }
-
-    private static ClientRegistration google() {
-        return CommonOAuth2Provider.GOOGLE.getBuilder("google")
-                .clientId("client-id")
-                .clientSecret("client-secret")
-                .build();
     }
 
 }
