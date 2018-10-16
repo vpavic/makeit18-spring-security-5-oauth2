14e696d265a840ab0f64e14dbc9803487c12cb8e
Vedran Pavic <vedran.pavic@gmail.com>
2018-10-16 06:58:43 +0200

    Add GitHub API call to OAuth2 client demo

--- demo-client/src/main/java/demo/DemoClientApplication.java
+++ demo-client/src/main/java/demo/DemoClientApplication.java
@@ -1,11 +1,14 @@
 package demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @RestController
@@ -18,6 +21,19 @@ public class DemoClientApplication {
     @GetMapping(path = "/")
     public AuthenticatedPrincipal getPrincipal(@AuthenticationPrincipal AuthenticatedPrincipal principal) {
         return principal;
+    }
+
+    @Autowired
+    private WebClient webClient;
+
+    @GetMapping(path = "/repos")
+    public String getRepos() {
+        return this.webClient.get()
+                .uri("https://api.github.com/user/repos")
+                .attributes(ServletOAuth2AuthorizedClientExchangeFilterFunction.clientRegistrationId("github"))
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
 
 }
--- demo-client/src/main/java/demo/SecurityConfiguration.java
+++ demo-client/src/main/java/demo/SecurityConfiguration.java
@@ -1,8 +1,13 @@
 package demo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -16,6 +21,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
                 .anyRequest().authenticated();
         // @formatter:on
+    }
+
+    @Bean
+    public WebClient webClient(ClientRegistrationRepository clientRegistrationRepository,
+                               OAuth2AuthorizedClientRepository authorizedClientRepository) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction exchangeFilterFunction =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository,
+                        authorizedClientRepository);
+        return WebClient.builder()
+                .apply(exchangeFilterFunction.oauth2Configuration())
+                .build();
     }
 
 }
